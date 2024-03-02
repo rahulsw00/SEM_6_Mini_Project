40c54d23395a2cb71952d513af0d0ee53944cf06
rahulsw00 <rahulsw12234@gmail.com>
2024-03-02 14:27:06 +0530

    Update main.py

--- main/main.py
+++ main/main.py
@@ -7,14 +7,15 @@ page_range = (1)
 top_img = []
 top_name = []
 top_price = []
-
+top_rate = []
+top_ref = []
 product = input("Enter product name: ")
 
 url = 'https://www.flipkart.com/search?q='
 eurl = '&otracker=search&otracker1=search&marketplace=FLIPKART&as-show=on&as=off&page='
 
 
-class webscreapping:
+class scraper:
 
     def __init__(self, url):
 
@@ -25,6 +26,8 @@ class webscreapping:
             names = soup.findAll('div', attrs={'class': '_4rR01T'})
             prices = soup.findAll('div', attrs={'class': '_30jeq3 _1_WHN1'})
             images = soup.findAll('img', attrs={'class': '_396cs4'})
+            ratings = soup.findAll('div', attrs={'class': '_3LWZlK'})
+            refs = soup.findAll('a', attrs={'class': '_1fQZEK'})
             for name in names:
                 top_name.append(name.text)
                 break
@@ -32,19 +35,27 @@ class webscreapping:
                 top_price.append(price.text)
                 break
             for img in images:
-                top_img.append(img)
+                top_img.append(img['src'])
+                break
+            for rate in ratings:
+                top_rate.append(rate.text)
+                break
+            for href in refs:
+                top_ref.append(href['href'])
                 break
             break
 
-def data(top_name, top_price, top_img):
+def data(top_name, top_price, top_img, top_rate, top_ref):
     df = pd.DataFrame()
     df['Name'] = top_name
     df['Price'] = top_price
     df['Image'] = top_img
+    df['Rating'] = top_rate
+    df['Href'] = top_ref
     print("Scrapping Done.......... ")
     print(df)
 
 
-ws1 = webscreapping(url)
-data(top_name, top_price, top_img)
+scraper(url)
+data(top_name, top_price, top_img, top_rate, top_ref)
 
