ad5bfbe4e015f0ed7743fe518b1c3e8f3c5a5ea4
rahulsw00 <rahulsw12234@gmail.com>
2024-03-24 19:10:01 +0530

    Update main.py

--- main/main.py
+++ main/main.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from flipkart import flipkart_scraper
+from croma import croma_scraper
 
 
 top_img = []
@@ -21,14 +22,24 @@ def data(top_name, top_price, top_img, top_rate, top_ref):
     print("Scrapping Done.......... ")
     print(df)
 
-class MainClass:
+class FlipClass:
     def __init__(self):
         self.flip_instance = flipkart_scraper(product)
-
+    
     def get_list_from_flipkart(self):
         return self.flip_instance.get_list()
+    
+class CromaClass:
+    def __init__(self):
+        self.croma_instance = croma_scraper(product)
+
+    def get_list_from_croma(self):
+        return self.croma_instance.get_lsit() 
 
 if __name__ == "__main__":
-    main_instance = MainClass()
-    flipkart_data = main_instance.get_list_from_flipkart()
+    croma_instance = CromaClass()
+    flip_instance = FlipClass()
+    flipkart_data = flip_instance.get_list_from_flipkart()
+    croma_data = croma_instance.get_list_from_croma()
     print(flipkart_data)
+    print(croma_data)
